263e2dac6ac0517e0f0a615478ae2b5900b7b7fe
Keerthimalini6 <keerthimalini6699@gmail.com>
2023-05-04 23:06:26 +0530

    Functionality

--- quizapp/src/components/Data/QuizData.js
+++ quizapp/src/components/Data/QuizData.js
@@ -1,22 +1,50 @@
-export const QuizData = [
-    {
-        question: "Which language runs in a web browser?",
-        options:[ "Java", "C", "Python", "JavaScript"],
-        answer: 4
-    },
-    {
-        question: "What does CSS stand for?",
-        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
-        answer: 2
-    },
-    {
-        question: "What does HTML stand for?",
-        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
-        answer: 1
-    },
-    {
-        question: "What year was JavaScript launched?",
-        options: ["1996", "1995", "1994", "none of the above"],
-        answer:2
-    },
+export const QuizData = [  {    question: "What is the virtual DOM in React?",    
+    options: [      
+      "A DOM that only exists in testing environments",      
+      "A DOM used for server-side rendering",      
+      "A JavaScript representation of the actual DOM",      
+      "A way to interact with DOM elements using voice commands"    
+    ],
+    answer: 3
+  },
+  {
+    question: "What is a stateful component in React?",
+    options: [
+      "A component that uses lifecycle methods",
+      "A component that doesn't use props",
+      "A component that stores data using a state object",
+      "A component that is rendered on the server-side"
+    ],
+    answer: 3
+  },
+  {
+    question: "What is JSX in React?",
+    options: [
+      "A syntax for defining HTML-like elements in JavaScript",
+      "A file format used for React components",
+      "A JavaScript framework for building user interfaces",
+      "A way to define styles in React components"
+    ],
+    answer: 1
+  },
+  {
+    question: "What is a higher-order component in React?",
+    options: [
+      "A component that renders other components",
+      "A component that can only be used once per application",
+      "A function that takes a component and returns a new component",
+      "A component that is used to manipulate the DOM"
+    ],
+    answer: 3
+  },
+  {
+    question: "What is Redux in React?",
+    options: [
+      "A way to handle data and state in React applications",
+      "A component that handles user input",
+      "A tool for testing React components",
+      "A package manager for React applications"
+    ],
+    answer: 1
+  }
 ];
